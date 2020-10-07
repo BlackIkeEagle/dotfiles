d51ebdadac48205b7dcd0a05f520fd48e8539a3c
BlackEagle <ike.devolder@gmail.com>
2020-10-07 16:55:49 +0200

    tmux :: enhanced vcs-status for hopefully better performance
    Signed-off-by: BlackEagle <ike.devolder@gmail.com>

--- .tmux/vcs-status.sh
+++ .tmux/vcs-status.sh
@@ -18,79 +18,79 @@ PSCOL='#[fg=#3971ed]'
 
 function vcsstatus {
     local active=$1
-    local vcsfound=0
 
     if [[ $active -ne 1 ]]; then
         return;
     fi
 
-    if [[ $(id -u) != "0" ]]; then
-        if [[ $vcsfound -ne 1 ]] && which git > /dev/null 2>&1; then
-            if git rev-parse > /dev/null 2>&1; then
-                GITBRANCH=$(git symbolic-ref HEAD 2>/dev/null)
-                GITBRANCH=${GITBRANCH/refs\/heads\//}
-                GITDIRTY=
-                if [[ $SCMDIRTY -eq 1 ]]; then
-                    # if has unstaged changes
-                    git diff --no-ext-diff --quiet --exit-code || GITDIRTY=" *"
-                    # if it has staged changes
-                    if [[ "$GITDIRTY" = "" ]]; then
-                        git diff --staged --no-ext-diff --quiet --exit-code || GITDIRTY=" +"
-                    fi
-                fi
-                if [[ "${GITBRANCH}" == "master" ]]; then
-                    GITBRANCH="${PSCOL}─(${COLYLW}git${PSCOL})─(${COLGRN}${GITBRANCH}${GITDIRTY}${PSCOL})"
-                elif [[ "${GITBRANCH}" == "" ]]; then
-                    GITBRANCH="${PSCOL}─(${COLYLW}git${PSCOL})─(${COLRED}$(git rev-parse --short HEAD)...${GITDIRTY}${PSCOL})"
-                else
-                    GITBRANCH="${PSCOL}─(${COLYLW}git${PSCOL})─(${COLCYN}${GITBRANCH}${GITDIRTY}${PSCOL})"
-                fi
-                printf "${GITBRANCH}"
-                vcsfound=1
+    if [[ $(id -u) == "0" ]]; then
+        return;
+    fi
+
+    GITENABLED=0
+    HGENABLED=0
+    SVNENABLED=0
+    BZRENABLED=0
+    if which git > /dev/null 2>&1; then
+        GITENABLED=1
+    fi
+    if which hg > /dev/null 2>&1; then
+        HGENABLED=1
+    fi
+    if which svn > /dev/null 2>&1; then
+        SVNENABLED=1
+    fi
+    if which bzr > /dev/null 2>&1; then
+        BZRENABLED=1
+    fi
+
+    if [[ $GITENABLED -eq 1 ]] && GITBRANCH=$(git rev-parse --abbrev-ref HEAD 2>&1); then
+        GITDIRTY=
+        if [[ $SCMDIRTY -eq 1 ]]; then
+            # if has unstaged changes
+            git diff --no-ext-diff --quiet --exit-code || GITDIRTY=" *"
+            # if it has staged changes
+            if [[ "$GITDIRTY" = "" ]]; then
+                git diff --staged --no-ext-diff --quiet --exit-code || GITDIRTY=" +"
             fi
         fi
-        if [[ $vcsfound -ne 1 ]] && which hg > /dev/null 2>&1; then
-            if hg branch > /dev/null 2>&1; then
-                HGBRANCH=$(hg branch 2>/dev/null)
-                HGDIRTY=
-                if [[ $SCMDIRTY -eq 1 ]]; then
-                    [[ "$(hg status -n | wc -l)" == "0" ]] || HGDIRTY=" *"
-                fi
-                if [[ "${HGBRANCH}" == "default" ]]; then
-                    HGBRANCH="${PSCOL}─(${COLYLW}hg${PSCOL})─(${COLGRN}${HGBRANCH}${HGDIRTY}${PSCOL})"
-                else
-                    HGBRANCH="${PSCOL}─(${COLYLW}hg${PSCOL})─(${COLRED}${HGBRANCH}${HGDIRTY}${PSCOL})"
-                fi
-                printf "${HGBRANCH}"
-                vcsfound=1
-            fi
+        if [[ "${GITBRANCH}" == "master" ]] || [[ "${GITBRANCH}" == "main" ]]; then
+            GITBRANCH="${PSCOL}─(${COLYLW}git${PSCOL})─(${COLGRN}${GITBRANCH}${GITDIRTY}${PSCOL})"
+        elif [[ "${GITBRANCH}" == "" ]]; then
+            GITBRANCH="${PSCOL}─(${COLYLW}git${PSCOL})─(${COLRED}$(git rev-parse --short HEAD)...${GITDIRTY}${PSCOL})"
+        else
+            GITBRANCH="${PSCOL}─(${COLYLW}git${PSCOL})─(${COLCYN}${GITBRANCH}${GITDIRTY}${PSCOL})"
         fi
-        if [[ $vcsfound -ne 1 ]] && which svn > /dev/null 2>&1; then
-            if svn info > /dev/null 2>&1; then
-                SVNREVISION=$(svn info | sed -ne 's/^Revision: //p')
-                if [[ $SCMDIRTY -eq 1 ]]; then
-                    [[ "$(svn status | wc -l)" == "0" ]] || SVNDIRTY=" *"
-                fi
-                SVNBRANCH="${PSCOL}─(${COLYLW}svn${PSCOL})─(${COLGRN}${SVNREVISION}${SVNDIRTY}${PSCOL})"
-                printf "${SVNBRANCH}"
-                vcsfound=1
-            fi
+        printf "%s" "${GITBRANCH}"
+    elif [[ $HGENABLED -eq 1 ]] && HGBRANCH=$(hg branch 2>/dev/null); then
+        HGDIRTY=
+        if [[ $SCMDIRTY -eq 1 ]]; then
+            [[ "$(hg status -n | wc -l)" == "0" ]] || HGDIRTY=" *"
         fi
-        if [[ $vcsfound -ne 1 ]] && which bzr > /dev/null 2>&1; then
-            if bzr nick > /dev/null 2>&1; then
-                BZRREVISION=$(bzr revno)
-                if [[ $SCMDIRTY -eq 1 ]]; then
-                    [[ "$(bzr status | wc -l)" == "0" ]] || BZRDIRTY=" *"
-                fi
-                BZRBRANCH="${PSCOL}─(${COLYLW}bzr${PSCOL})─(${COLGRN}${BZRREVISION}${BZRDIRTY}${PSCOL})"
-                printf "${BZRBRANCH}"
-                vcsfound=1
-            fi
+        if [[ "${HGBRANCH}" == "default" ]]; then
+            HGBRANCH="${PSCOL}─(${COLYLW}hg${PSCOL})─(${COLGRN}${HGBRANCH}${HGDIRTY}${PSCOL})"
+        else
+            HGBRANCH="${PSCOL}─(${COLYLW}hg${PSCOL})─(${COLRED}${HGBRANCH}${HGDIRTY}${PSCOL})"
         fi
+        printf "%s" "${HGBRANCH}"
+    elif [[ $SVNENABLED -eq 1 ]] && SVNINFO=$(svn info 2>&1); then
+        SVNREVISION=$(echo "$SVNINFO" | sed -ne 's/^Revision: //p')
+        if [[ $SCMDIRTY -eq 1 ]]; then
+            [[ "$(svn status | wc -l)" == "0" ]] || SVNDIRTY=" *"
+        fi
+        SVNBRANCH="${PSCOL}─(${COLYLW}svn${PSCOL})─(${COLGRN}${SVNREVISION}${SVNDIRTY}${PSCOL})"
+        printf "%s" "${SVNBRANCH}"
+    elif [[ $BZRENABLED -eq 1 ]] && bzr nick > /dev/null 2>&1; then
+        BZRREVISION=$(bzr revno)
+        if [[ $SCMDIRTY -eq 1 ]]; then
+            [[ "$(bzr status | wc -l)" == "0" ]] || BZRDIRTY=" *"
+        fi
+        BZRBRANCH="${PSCOL}─(${COLYLW}bzr${PSCOL})─(${COLGRN}${BZRREVISION}${BZRDIRTY}${PSCOL})"
+        printf "%s" "${BZRBRANCH}"
     fi
 }
 
 (
-    cd $1
-    vcsstatus $2
+    cd "$1" || exit 1
+    vcsstatus "$2"
 )
